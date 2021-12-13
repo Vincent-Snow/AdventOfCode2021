832ac9955acc33fd5420f1ca0e96a997b1d876d6
Vincent Snow <vinsnow@extendhealth.com>
2021-12-13 09:52:06 -0700

    Day 5 part 2 solved

--- Day5/src/com/company/Main.java
+++ Day5/src/com/company/Main.java
@@ -13,18 +13,13 @@ public class Main {
         HashMap<String,Integer> points = new HashMap<String,Integer>();
         int c = 0;
         bLoop: for (String i : boards)  {
-            System.out.println(i);
             String coords[] = i.split(" -> ");
-            int x1;
-            int y1;
-            int x2;
-            int y2;
             String one[] = coords[0].split(",");
             String two[] = coords[1].split(",");
-            x1 = Integer.parseInt(one[0]);
-            y1 = Integer.parseInt(one[1]);
-            x2 = Integer.parseInt(two[0]);
-            y2 = Integer.parseInt(two[1]);
+            int x1 = Integer.parseInt(one[0]);
+            int y1 = Integer.parseInt(one[1]);
+            int x2 = Integer.parseInt(two[0]);
+            int y2 = Integer.parseInt(two[1]);
             if (((x1 == x2) == false) && ((y1 == y2) == false)) {
                 continue bLoop;
             }
@@ -69,7 +64,55 @@ public class Main {
     }
 
     static void ptTwo() {
+        String boards[] = Input.input.split("\\r?\\n");
+        HashMap<String,Integer> points = new HashMap<String,Integer>();
+        int c = 0;
+        bLoop: for (String i : boards)  {
+            String coords[] = i.split(" -> ");
+            String one[] = coords[0].split(",");
+            String two[] = coords[1].split(",");
+            int x1 = Integer.parseInt(one[0]);
+            int y1 = Integer.parseInt(one[1]);
+            int x2 = Integer.parseInt(two[0]);
+            int y2 = Integer.parseInt(two[1]);
 
+            int inc = Math.abs(x1-x2);
+            if (x1 == x2) {
+                inc = Math.abs(y1-y2);
+            } else if (y1==y2) {
+                inc = Math.abs(x1-x2);
+            }
+            for (int x = 0; x <= inc; x++) {
+                String p = "";
+                String xp = "";
+                String yp = "";
+                if (x1 == x2) {
+                    xp = String.valueOf(x1);
+                } else if (x1 > x2) {
+                    xp = String.valueOf(x1 -x);
+                } else if (x1 < x2) {
+                    xp = String.valueOf(x1+x);
+                }
+                if (y1 == y2) {
+                    yp = String.valueOf(y1);
+                } else if (y1 > y2) {
+                    yp = String.valueOf(y1-x);
+                } else if (y1 < y2) {
+                    yp = String.valueOf(y1+x);
+                }
+                p = xp + "," + yp;
+                if (points.containsKey(p) == true) {
+                    points.put(p, points.get(p) + 1);
+                } else {
+                    points.put(p,1);
+                }
+            }
+
+        }
+
+        for (String i : points.keySet())
+            if (points.get(i) >= 2) c++;
+        System.out.println(c);
 
     }
 }
