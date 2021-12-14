d3fdd17f8d6e868dd7fb020e61d2866b18160d83
Vincent Snow <vinsnow@extendhealth.com>
2021-12-14 14:24:13 -0700

    Day 6 part 2 solved!

--- Day6/src/com/company/Main.java
+++ Day6/src/com/company/Main.java
@@ -32,28 +32,43 @@ public class Main {
 
     static void ptTwo() {
 
-        String arrFishes[] = Input.testInput2.split(",");
+        String arrFishes[] = Input.input.split(",");
         List<String> fishes = new ArrayList<String>();
+        HashMap<Integer,Long> fishDays = new HashMap<Integer, Long>();
         Collections.addAll(fishes,arrFishes);
-        int i = 0;
-        while (i < 100) {
-
-            int size = fishes.size();
-            for (int ind = 0; ind < size; ind++) {
-                if (fishes.get(ind).equals(("0"))) {
-                    fishes.add("8");
-                    fishes.set(ind,"6");
-                } else {
-                    int fishInt = Integer.parseInt(fishes.get(ind))-1;
-                    String fishStr = String.valueOf(fishInt);
-                    fishes.set(ind,fishStr);
-                }
-            }
-            i++;
-            System.out.println(String.valueOf(fishes.size()) + " " + i);
-            //System.out.println(fishes);
+        for (int i = 0; i < 9; i++) {
+            fishDays.put(i, 0L);
+        }
+        for (String fish : arrFishes) {
+            Integer intFish = Integer.parseInt(fish);
+            fishDays.put(intFish, fishDays.get(intFish) + 1);
 
         }
-        System.out.println(fishes.size());
+        System.out.println(fishDays);
+        int i = 0;
+
+        while (i < 256) {
+
+            int size = fishDays.size();
+            HashMap<Integer,Long> fishOfTheDay = new HashMap<Integer,Long>();
+            for (int d = size-1; d >= 0; d--) {
+                Long daily = 0L;
+                if (d != 0) {
+                    daily = fishDays.get(d);
+                    fishOfTheDay.put(d-1,daily);
+                } else if (d == 0) {
+                    daily = fishDays.get(d);
+                    fishOfTheDay.put(6,fishOfTheDay.get(6) + daily);
+                    fishOfTheDay.put(8,daily);
+                }
+            }
+            fishDays = fishOfTheDay;
+                        i++;
+        }
+        Long c = 0L;
+        for (Long fishCount: fishDays.values()) {
+            c+=fishCount;
+        }
+        System.out.println(c);
     }
 }
